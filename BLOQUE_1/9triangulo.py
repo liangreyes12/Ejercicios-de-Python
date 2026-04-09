2b51585b5a2ce59f7b7a879fca8ebbc1c7a99f1d
Liang <jhonwoodz675@gmail.com>
2026-04-09 17:08:04 -0500

    Se crea ek ejercicio 9 del bloque 1, basado en el analisis matematico de numeros para triangulos

---
+++ BLOQUE_1/9triangulo.py
@@ -0,0 +1,11 @@
+"""Dadas tres longitudes (números reales positivos)
+- Determina si pueden formar un triángulo válido.
+- En caso afirmativo, clasifica el triángulo por sus lados
+(equilátero, isósceles o escaleno)
+
+- También por sus ángulos (acutángulo, rectángulo u obtusángulo).
+- Implementa la solución con validación de entrada robusta 
+y eficiencia computacional."""
+
+print(f"===ANALISIS NUEMRICO DE TRIANGULOS===")
+
